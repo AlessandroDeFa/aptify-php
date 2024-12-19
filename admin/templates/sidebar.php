682e494bb5205b8ce74e4266c07be00d90c9429c
Alessandro De Faveri <aledefaale@gmail.com>
2024-12-19 16:58:21 +0100

    updated

--- admin/templates/sidebar.php
+++ admin/templates/sidebar.php
@@ -6,12 +6,12 @@
             'contents' => [
                 'label' => 'I tuoi contenuti',
                 'icon' => '<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M12 19l7-7 3 3-7 7-3-3z"></path><path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"></path><path d="M2 2l7.586 7.586"></path><circle cx="11" cy="11" r="2"></circle></svg>',
-                'href' => $fullUrl . '/aptify/admin/contents'
+                'href' => $fullUrl . '/admin/contents'
             ],
             'gallery' => [
                 'label' => 'Galleria',
                 'icon' => '<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>',
-                'href' => $fullUrl . '/aptify/admin/gallery'
+                'href' => $fullUrl . '/admin/gallery'
             ]
         ];
 
